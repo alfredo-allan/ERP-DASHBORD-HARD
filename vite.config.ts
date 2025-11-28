a87c43e0ff68b7af0ed8a01b49721c63c974205c
Alfredo Allan <allan-mecanico@outlook.com>
2025-11-28 17:37:34 -0300

    Executando build

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    sourcemap: false,
-    chunkSizeWarningLimit: 1600,
-  },
-  server: {
-    port: 3000,
-    host: true,
-  },
-  preview: {
-    port: 3000,
-    host: true,
-  },
+  base: "/", // ESSENCIAL NA VERCEL
 });
-/* testeando build */
